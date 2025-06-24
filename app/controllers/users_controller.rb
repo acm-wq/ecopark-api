98a6f531c56f910cd81837e1dcac6be2132b11eb
acm-wq <camisglhdadae@gmail.com>
2025-06-25 00:37:59 +0700

    main 🌱 Fix users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,6 @@
-require 'rails_helper'
-require 'swagger_helper'
-
-RSpec.describe 'Users API', type: :request do
-  path '/users' do
-    get 'Retrieves a list of users' do
-      tags 'Users'
-      produces 'application/json'
-
-      response '200', 'users found' do
-        schema type: :array,
-               items: {
-                 type: :object,
-                 properties: {
-                   id: { type: :integer },
-                   email: { type: :string, format: :email },
-                   password_digest: { type: :string },
-                   created_at: { type: :string, format: 'date-time' },
-                   updated_at: { type: :string, format: 'date-time' }
-                 },
-                 required: ['id', 'email', 'password_digest', 'created_at', 'updated_at']
-               }
-
-        before do
-          create_list(:user, 3)
-        end
-
-        run_test!
-      end
-    end
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users, status: :ok
   end
 end
