8b2e6f4b54b045dc869c9118470586844fa8b695
acm-wq <camisglhdadae@gmail.com>
2025-06-25 14:59:15 +0700

    #3 🌱 Add test for validation user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with a duplicate email" do
+      User.create(email: "testuser", password: "password")
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with an invalid email format" do
+      subject.email = "invalid_email"
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid if the password is too short" do
+      subject.password = "short"
+      expect(subject).to_not be_valid
+    end
+  end
 end
