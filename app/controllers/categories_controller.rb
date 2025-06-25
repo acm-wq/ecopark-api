9dd8e7b4d2401133f5c59812bb320fd2ca5b57c1
acm-wq <camisglhdadae@gmail.com>
2025-06-25 18:56:19 +0700

    #6 🌱 Refactoring method index in categories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,42 +5,43 @@ class CategoriesController < ApplicationController
 
   # GET /api/categories
   def index
-    type = params.require(:category).permit(:type)[:type]
-
-    case type
+    case params.require(:category).permit(:type)[:type]
     when 'all'
       pagy_cat, categories = pagy(Category.all, page_param: :categories_page)
       pagy_subcat, subcategories = pagy(SubCategory.all, page_param: :subcategories_page)
-
       render json: {
         categories: ActiveModelSerializers::SerializableResource.new(
-          categories, each_serializer: CategorySerializer
+          categories,
+          each_serializer: CategorySerializer
         ),
         categories_pagy: pagy_metadata(pagy_cat),
-
         subcategories: ActiveModelSerializers::SerializableResource.new(
-          subcategories, each_serializer: SubCategorySerializer
+          subcategories,
+          each_serializer: SubCategorySerializer
         ),
         subcategories_pagy: pagy_metadata(pagy_subcat)
       }, status: :ok
     when 'category'
-      pagy_obj, categories = pagy(Category.all)
-      render json: {
-        categories: ActiveModelSerializers::SerializableResource.new(
-          categories, each_serializer: CategorySerializer
-        ),
-        pagy: pagy_metadata(pagy_obj)
+      render json: pagy(Category.all).then { |pagy_obj, categories|
+        {
+          categories: ActiveModelSerializers::SerializableResource.new(
+            categories,
+            each_serializer: CategorySerializer
+          ),
+          pagy: pagy_metadata(pagy_obj)
+        }
       }, status: :ok
-
     when 'subcategory'
-      pagy_obj, subcategories = pagy(SubCategory.all)
-      render json: {
-        subcategories: ActiveModelSerializers::SerializableResource.new(
-          subcategories, each_serializer: SubCategorySerializer
-        ),
-        pagy: pagy_metadata(pagy_obj)
+      render json: pagy(SubCategory.all).then { |pagy_obj, subcategories|
+        {
+          subcategories: ActiveModelSerializers::SerializableResource.new(
+            subcategories, 
+            each_serializer: 
+            SubCategorySerializer
+          ),
+          pagy: pagy_metadata(pagy_obj)
+        }
       }, status: :ok
-
     else
       render json: { error: 'Invalid category type' }, status: :bad_request
     end
